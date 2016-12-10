cef931198b64bc12cd80c0a42aa33cea79c90cd1
LibuMathew <libu.mvk@gmail.com>
2016-12-11 01:41:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var port=Number(process.env.PORT || 3000);
+
 app.use(express.static('public'))
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000);
+app.listen(port);
